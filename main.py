6862737ec34c74fd47e480043832dce5c32eef17
Haydens-workshop <76880991+Haydens-workshop@users.noreply.github.com>
2023-03-28 01:26:27 -0400

    Update main.py
    updates for kicks and giggles.
    When the PySimpleGUI window is closed, the read() method returns a tuple with two elements: the event that was triggered (in this case, either "OK" or "Cancel") and a dictionary with the input values
    
    The read() method replaced with (). --  read() is no longer a valid method for creating a PySimpleGUI window.
    
    The email and password variables were being overwritten with Selenium web elements.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import os
 import shutil
 
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
+PATH = "C:\\Program Files (x86)\\chromedriver.exe"
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--disable-user-media-security=true")
@@ -25,65 +25,71 @@ options.add_argument("--disable-popup-blocking")
 driver = webdriver.Chrome(PATH, options=options)
 action = ActionChains(driver)
 
-event, values = sg.Window('AMD BUYER BOT(Type all information for your bestbuy acct!)',
+window = sg.Window('AMD BUYER BOT(Type all information for your bestbuy acct!)',
                   [[sg.T('Enter Your First Name'), sg.In(key='-ID-')],
                   [sg.T('Enter your Last name'), sg.In(key='-ID-1')],
                   [sg.T('Enter your Best Buy Email'), sg.In(key='-ID-2')],
                    [sg.T('Enter your Best Buy Password'), sg.In(key='-ID-3')],
                   [sg.T('Enter your Credit Card CVV'), sg.In(key='-ID-4')],
-                  [sg.B('OK'), sg.B('Cancel') ]]).read(close=True)
+                  [sg.B('OK'), sg.B('Cancel')]])
 
-first_Name = values['-ID-']
-last_Name=values['-ID-1']
-email=values['-ID-2']
-password=values['-ID-3']
-cvv=values['-ID-4']
+event, values = window.read()
 
+if event == 'OK':
+    first_Name = values['-ID-']
+    last_Name=values['-ID-1']
+    bb_email=values['-ID-2']
+    bb_password=values['-ID-3']
+    cvv=values['-ID-4']
 
+    driver.get("https://www.bestbuy.com/site/xfx-amd-radeon-rx-6700-xt-12gb-gddr6-pci-express-4-0-gaming-graphics-card-gray-black/6457624.p?skuId=6457624")
 
-driver.get("https://www.bestbuy.com/site/xfx-amd-radeon-rx-6700-xt-12gb-gddr6-pci-express-4-0-gaming-graphics-card-gray-black/6457624.p?skuId=6457624")
-
-
-driver.maximize_window()
-time.sleep(5)
-driver.find_element_by_xpath("//*[contains(text(), 'United States')]").click()
-time.sleep(3)
-element = driver.find_element_by_class_name('add-to-cart-button')
-a=1
-while a==1:
+    driver.maximize_window()
+    time.sleep(5)
+    driver.find_element_by_xpath("//*[contains(text(), 'United States')]").click()
+    time.sleep(3)
     element = driver.find_element_by_class_name('add-to-cart-button')
-    data = element.is_enabled()
-    if data == True:
-        element.click()
-        time.sleep(3)
-        driver.get("https://www.bestbuy.com/cart")
-        time.sleep(2)
-        driver.find_element_by_xpath("//*[contains(text(), 'Checkout')]").click()
-        time.sleep(3)
-        email = driver.find_element_by_id("fld-e")
-        password= driver.find_element_by_id("fld-p1")
-
-        email.send_keys(email)
-        password.send_keys(password)
-        driver.find_element_by_class_name("btn-secondary").click()
-        time.sleep(5)
-        driver.find_element_by_class_name("ispu-card__switch").click()
-        time.sleep(6)
-        driver.find_element_by_xpath("//*[contains(text(), 'Continue to Payment Information')]").click()
-        time.sleep(6)
-        #final payment page
-        cvv= driver.find_element_by_id("credit-card-cvv")
-        cvv.send_keys(cvv)
-        time.sleep(3)
-        driver.find_element_by_class_name('btn-primary').click()
-        a=2
-    else:
-        time.sleep(420)
-        driver.get("https://www.bestbuy.com/site/xfx-amd-radeon-rx-6700-xt-12gb-gddr6-pci-express-4-0-gaming-graphics-card-gray-black/6457624.p?skuId=6457624")
-        try:
-            time.sleep(5)
-            driver.find_element_by_xpath("//*[contains(text(), 'United States')]").click()
+    a=1
+    while a==1:
+        element = driver.find_element_by_class_name('add-to-cart-button')
+        data = element.is_enabled()
+        if data == True:
+            element.click()
             time.sleep(3)
-            element = driver.find_element_by_class_name('add-to-cart-button')
-        except:
-            pass
+            driver.get("https://www.bestbuy.com/cart")
+            time.sleep(2)
+            driver.find_element_by_xpath("//*[contains(text(), 'Checkout')]").click()
+            time.sleep(3)
+            email_field = driver.find_element_by_id("fld-e")
+            password_field = driver.find_element_by_id("fld-p1")
+
+            email_field.send_keys(bb_email)
+            password_field.send_keys(bb_password)
+            driver.find_element_by_class_name("btn-secondary").click()
+            time.sleep(5)
+            driver.find_element_by_class_name("ispu-card__switch").click()
+            time.sleep(6)
+            driver.find_element_by_xpath("//*[contains(text(), 'Continue to Payment Information')]").click()
+            time.sleep(6)
+            #final payment page
+            cvv_field= driver.find_element_by_id("credit-card-cvv")
+            cvv_field.send_keys(cvv)
+            time.sleep(3)
+            driver.find_element_by_class_name('btn-primary').click()
+            a=2
+        else:
+            time.sleep(420)
+            driver.get("https://www.bestbuy.com/site/xfx-speedster-swft309-amd-radeon-rx-6700xt-12gb-gddr6-pci-express-4-0-gaming-graphics-card-black/6513905.p?skuId=6513905")
+            try:
+                time.sleep(5)
+                driver.find_element_by_xpath("//*[contains(text(), 'United States')]").click()
+                time.sleep(3)
+                element = driver.find_element_by_class_name('add-to-cart-button')
+            except:
+                pass
+
+    driver.close()
+
+else:
+    window.close()
+
